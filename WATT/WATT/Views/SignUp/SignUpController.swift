9a58b26b1d865ba6f0765e1cae94a14fd4712cde
boikostas <boikostas3@gmail.com>
2024-02-11 15:15:54 +0200

    bindSecureFieldPublishers() in viewDidLoad

--- WATT/WATT/Views/SignUp/SignUpController.swift
+++ WATT/WATT/Views/SignUp/SignUpController.swift
@@ -35,6 +35,7 @@ class SignUpController: UIViewController {
         setupTarget()
         bindViewToViewModel()
         bindViewModelToView()
+        bindSecureFieldPublishers()
     }
     
     private func setupTarget() {
